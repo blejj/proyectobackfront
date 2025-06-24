df21755efff031e466370ca63732156c00abe592
blejj <fbleileee@gmail.com>
2025-06-23 23:39:01 -0300

    Arreglo del Auth

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogoComponent } from './home/sections/catalogo/catalogo.component';
-import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'catalogo', component: CatalogoComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent }
 ];
