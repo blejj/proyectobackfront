59ddd5041eb3a73ad4ccdc891352d74561b7b7c9
blejj <fbleileee@gmail.com>
2025-06-24 21:53:04 -0300

    Una vez registrado el usuario, enviar al login

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { RegisterUser } from '../../models/register.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -22,12 +23,18 @@ export class RegisterComponent {
     dni: ''
   };
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   onRegister() {
-    this.authService.register(this.user).subscribe({
-      next: res => console.log('Registro exitoso:', res),
-      error: err => console.error('Error en registro:', err)
-    });
-  }
+  this.authService.register(this.user).subscribe({
+    next: res => {
+      console.log('Registro exitoso:', res);
+      this.router.navigate(['/login']); // Redirección al login
+    },
+    error: err => {
+      console.error('Error en registro:', err);
+      alert('Error al registrarse. Por favor, intentá nuevamente.');
+    }
+  });
+}
 }
