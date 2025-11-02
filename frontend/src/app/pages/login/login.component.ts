5aa8e6d1ba0309ec017413b318c7e088c3a717cd
blejj <fbleileee@gmail.com>
2025-11-02 16:21:34 -0300

    .

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -16,15 +16,19 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     const clientId = '1951tqfvb7fakucpruls1e1875';
-    const redirectUri = 'https://main.d17jgtfjujlttk.amplifyapp.com/home';
     const responseType = 'code';
     const scope = 'openid profile email';
-
     const cognitoDomain = 'https://us-east-1dvurikhle.auth.us-east-1.amazoncognito.com';
+
+    // Detecta si estás en localhost o en producción
+    const isLocal = window.location.hostname === 'localhost';
+
+    const redirectUri = isLocal
+      ? 'http://localhost:4200/home' // para pruebas locales
+      : 'https://main.d17jgtfjujlttk.amplifyapp.com/home'; // deploy Amplify
 
     const url = `${cognitoDomain}/login?client_id=${clientId}&response_type=${responseType}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
-    // Redirige automáticamente al Hosted UI
     window.location.href = url;
   }
 }
